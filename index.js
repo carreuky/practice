50c2dae53e8f4d7e407ab756020183c4e5869938
carreuky <carreuky@gmail.com>
2022-06-04 18:20:37 +0300

    function to display clicked number

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function clearScreen() {
     document.getElementsByClassName("display")[0].value = "";
    }
+   function display(number) {
+    document.getElementsByClassName("display")[0].value += number;
+   }
